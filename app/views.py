83d5f8e60848f0d079f9dd1b2c5c0d718bd0d883
Your Name <you@example.com>
2021-05-22 07:21:34 +0000

    init

--- app/views.py
+++ app/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.views.generic import View
+from .models import Profile
+
+class IndexView(View):
+    def get(self,request,request,*args,**kwargs)
+        profile_data=Plofile.objects.all()
+        if profile_data.exists():
+            profile_data=profile_data.order_by('-id')[0]
+        return render(request,'app/index.html'),{
+            'profile_data':profile_data
+        })
+        
 
 # Create your views here.
+
